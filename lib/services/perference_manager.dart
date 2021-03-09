08da478ffdd3e7ef2a3ec055a876187a0a64ebec
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2021-03-09 18:31:02 +0200

    Update perference_manager.dart

--- lib/services/perference_manager.dart
+++ lib/services/perference_manager.dart
@@ -11,14 +11,15 @@ class PrefManager {
   var password = "";
   var lang = 0;
   var pref;
-  var selectedAddress = 0;
+  String selectedAddress = "";
+  String type = '';
 
   PrefManager() {
     //loadDataFromLocalMemory();
   }
 
   Future<bool> setLoggedInData(String id, String name, String phone,
-      String email, String password) async {
+      String email, String password, String type) async {
     if (pref == null) {
       pref = await SharedPreferences.getInstance();
     }
@@ -27,11 +28,13 @@ class PrefManager {
     this.phone = phone;
     this.email = email;
     this.password = password;
+    this.type = type;
     pref.setString("id", id);
     pref.setString("name", name);
     pref.setString("phone", phone);
     pref.setString("email", email);
     pref.setString("password", password);
+    pref.setString("type", type);
     print("Saved! - {$id} - {$name} - {$email}");
     return true;
   }
@@ -46,7 +49,8 @@ class PrefManager {
     phone = pref.getString("phone") ?? phone;
     email = pref.getString("email") ?? email;
     password = pref.getString("password") ?? password;
-    selectedAddress = pref.getInt("address") ?? selectedAddress;
+    selectedAddress = pref.getString("address") ?? selectedAddress;
+    type = pref.getString("type") ?? type;
     print(
         "Loaded - {$id} - {$name} - {$email} - {Lang: $lang} - {Add: $selectedAddress}");
   }
@@ -60,7 +64,8 @@ class PrefManager {
     pref.setString("phone", phone);
     pref.setString("email", email);
     pref.setString("password", password);
-    pref.setInt("address", selectedAddress);
+    pref.setString("address", selectedAddress);
+    pref.setString("type", type);
     print("Saved! - {$id} - {$name} - {$email} - {$selectedAddress}");
   }
 
@@ -89,6 +94,11 @@ class PrefManager {
     return true;
   }
 
+  bool setType(String type) {
+    this.type = type;
+    return true;
+  }
+
   String getId() {
     return id;
   }
@@ -103,6 +113,10 @@ class PrefManager {
 
   String getEmail() {
     return email;
+  }
+
+  String getType() {
+    return type;
   }
 
   bool isLoggedIn() {
@@ -123,7 +137,7 @@ class PrefManager {
     pref.setString("phone", "");
     pref.setString("email", "");
     pref.setString("password", "");
-    pref.setInt("address", 0);
+    pref.setString("address", "");
   }
 
   Future<void> setLang(int lang) async {
@@ -142,21 +156,21 @@ class PrefManager {
     return (lang != 0);
   }
 
-  Future<void> setSelectedAddress(int address) async {
+  Future<void> setSelectedAddress(String address) async {
     if (pref == null) {
       pref = await SharedPreferences.getInstance();
     }
     this.selectedAddress = address;
-    pref.setInt("address", address);
+    pref.setString("address", address);
     updateDeliveryFeesAndTime();
   }
 
-  int getSelectedAddress() {
+  String getSelectedAddress() {
     return this.selectedAddress;
   }
 
   void updateDeliveryFeesAndTime() {
-    if (selectedAddress != 0) {
+    if (selectedAddress != "") {
       for (UserAddressesClass i in DataManager.userAddresses) {
         if (i.id == DataManager.mPrefManager.getSelectedAddress()) {
           CartManager.deliveryFees = i.locationData.deliveryFees;
