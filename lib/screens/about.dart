3ed9cfce237f27de93e5022c05c4d27b766f5593
youssefdegweyy <youssefdegweyy@hotmail.com>
2021-03-10 02:13:10 +0200

    Update about.dart

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -31,9 +31,39 @@ class _AboutState extends State<AboutUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('About Us'),
-      ),
+        appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0),
+    child: AppBar(
+    brightness: Brightness.dark,
+    flexibleSpace: Container(
+    decoration: new BoxDecoration(
+    gradient: LinearGradient(
+    colors: <Color>[
+    Color(0xFF00d466),
+    Color(0xFF00af87),
+    ],
+    ),
+    ),
+    ),
+    ),
+    ),
+    body: Scaffold(
+    appBar: AppBar(
+    toolbarHeight: 65,
+    automaticallyImplyLeading: false,
+    leadingWidth: 65,
+    title: Text(
+    'About Us',
+    style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    leading: Padding(
+    padding: EdgeInsets.only(left: 15, right: 9, top: 9, bottom: 9),
+    child: IconButton(
+    icon: Image.asset('assets/images/other_icons/back_arrow.png'),
+    onPressed: () => Navigator.of(context).pop(),
+    ),
+    ),
+    ),
       body: ListView(
         children: [
           SizedBox(height: 25),
@@ -78,6 +108,6 @@ class _AboutState extends State<AboutUs> {
           ),
         ],
       ),
-    );
+    ));
   }
 }
